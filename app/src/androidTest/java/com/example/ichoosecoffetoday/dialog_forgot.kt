bfdaf8ce971cf420bc0687ea4d4d2c08e4ae4608
ridho60 <154666893+ridho60@users.noreply.github.com>
2023-12-26 23:10:47 +0700

    Create dialog_forgot.kt

---
+++ app/src/androidTest/java/com/example/ichoosecoffetoday/dialog_forgot.kt
@@ -0,0 +1,11 @@
+package com.example.loginsignup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class dialog_forgot : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dialog_forgot)
+    }
+}
